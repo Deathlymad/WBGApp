fbbd9917e92838e9d3fd48fcef66867867ba1535
Deathlymad <meyerphilip96@googlemail.com>
2016-03-22 10:47:15 +0100

    a few quick lines fro substitute Plan and Event.

--- app/src/main/java/com/webteam/wbgapp/wbgapp/structure/Event.java
+++ app/src/main/java/com/webteam/wbgapp/wbgapp/structure/Event.java
@@ -1,11 +1,18 @@
 package com.webteam.wbgapp.wbgapp.structure;
 
+import android.app.Activity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.webteam.wbgapp.wbgapp.R;
 import com.webteam.wbgapp.wbgapp.net.DatabaseHandler;
 import com.webteam.wbgapp.wbgapp.net.IRequest;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -13,16 +20,20 @@ import java.util.Date;
  */
 public class Event implements IRequest {
     private int _id;
-    private int _addTime; //WHAT IS THIS
+    private Date _addTime; //WHAT IS THIS
     private Date _startTime, _endTime;
     private String _title;
 
     private int _author;
     private String _teaser;
     private String _location;
+
+    View _event;
+
+
     public Event(JSONObject data) throws JSONException {
         _id = data.getInt("id");
-        _addTime = data.getInt("addTime");
+        _addTime = Util.getDateFromTStamp(data.getInt("addTime"));
         _startTime = Util.getDateFromTStamp(data.getLong("startTime"));
         _endTime = Util.getDateFromTStamp(data.getLong("endTime"));
         _title = Util.unescUnicode(data.getString("title"));
@@ -40,10 +51,49 @@ public class Event implements IRequest {
         try {
             JSONObject extData = new JSONObject(res);
             _author = extData.getInt("author");
-            _teaser = extData.getString("teaser");
-            _location = extData.getString("location");
+            _teaser = Util.unescUnicode(extData.getString("teaser"));
+            _location = Util.unescUnicode(extData.getString("location"));
         } catch (JSONException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public String toString()
+    {
+        JSONObject obj = new JSONObject();
+        try {
+            obj.put("id", _id);
+            obj.put("addTime", Util.getTStampFromDate(_addTime.getTime()).getTime());
+            obj.put("startTime", Util.getTStampFromDate(_startTime.getTime()).getTime());
+            obj.put("endTime", Util.getTStampFromDate(_endTime.getTime()).getTime());
+            obj.put("title", Util.escUnicode(_title));
+            obj.put("author", _author);
+            obj.put("teaser", Util.escUnicode(_teaser));
+            obj.put("location", Util.escUnicode(_location));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return obj.toString();
+    }
+
+    public String getTitle()
+    {
+        return _title;
+    }
+
+    public <T extends View.OnClickListener> void addView(T view) {
+        LinearLayout container = (LinearLayout)((Activity)view).findViewById(R.id.news_container);
+        _event = ((Activity)view).getLayoutInflater().inflate(R.layout.activity_news_element, null);
+
+        TextView _teaser = (TextView)_event.findViewById(R.id.article_element_title);
+        _teaser.setText(_title);
+        _teaser.setOnClickListener(view);
+
+        TextView _teaserDate = (TextView)_event.findViewById(R.id.article_element_date);//TODO: needs to Change
+        String date = new SimpleDateFormat("yyyy-MM-dd").format(_startTime.getTime());
+        _teaserDate.setText(date);
+
+        container.addView(_event);
     }
 }
--- app/src/main/java/com/webteam/wbgapp/wbgapp/structure/SubstitutePlan.java
+++ app/src/main/java/com/webteam/wbgapp/wbgapp/structure/SubstitutePlan.java
@@ -1,12 +1,40 @@
 package com.webteam.wbgapp.wbgapp.structure;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+import java.util.List;
 
 /**
  * Created by Deathlymad on 20.03.2016 .
  */
 public class SubstitutePlan {
+    private class SubstituteEntry
+    {
+        String grade, formselector, subject,room, commentary;
+        String newSubject, newRoom;
+
+        SubstituteEntry(Node n)
+        {
+
+        }
+    }
+
+    private List<SubstituteEntry> plan;
+
     public SubstitutePlan(Document xmlSubstitues) {
+        Element e = xmlSubstitues.getDocumentElement();
         //TODO: Wenn wir Plan haben xD
+    }
+
+    public void getSubstitutionByClass()
+    {
+
+    }
+
+    public void getSubstitutionByPlan()
+    {
+
     }
 }
