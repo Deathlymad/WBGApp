0745c2ec5a0d2faf4d9c0953005b2628455aabb7
Deathlymad <meyerphilip96@googlemail.com>
2016-03-29 17:59:00 +0200

    Mailbox - Now Working

--- app/src/main/java/com/webteam/wbgapp/wbgapp/activity/MailInbox.java
+++ app/src/main/java/com/webteam/wbgapp/wbgapp/activity/MailInbox.java
@@ -1,5 +1,9 @@
 package com.webteam.wbgapp.wbgapp.activity;
 
+import android.os.Bundle;
+
+import com.webteam.wbgapp.wbgapp.R;
+
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -8,9 +12,17 @@ import java.io.IOException;
  * Created by malte on 29.03.2016.
  */
 public class MailInbox extends BaseActivity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mail_inbox);
+    }
+
     @Override
     protected String getName() {
-        return "@string/mail_inbox";
+        return getString(R.string.mail_inbox);
     }
 
     @Override
