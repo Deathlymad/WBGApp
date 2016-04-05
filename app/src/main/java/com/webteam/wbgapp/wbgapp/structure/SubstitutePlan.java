7beff7eeeb755529fdfd6e1144357a7dc87d6017
Deathlymad <meyerphilip96@googlemail.com>
2016-04-05 23:45:26 +0200

    SubstitutePlan now displaying. Malte FORMAT IT

--- app/src/main/java/com/webteam/wbgapp/wbgapp/structure/SubstitutePlan.java
+++ app/src/main/java/com/webteam/wbgapp/wbgapp/structure/SubstitutePlan.java
@@ -6,18 +6,14 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import java.io.FileOutputStream;
 import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
-
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
 /**
  * Created by Deathlymad on 20.03.2016 .
