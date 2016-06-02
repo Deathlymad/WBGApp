32d4b08337db4b1fc4d03b8414952b84374d4b23
metze <malte.metze@outlook.de>
2016-06-02 16:49:59 +0200

    Notification layout

--- app/src/main/java/com/webteam/wbgapp/wbgapp/activity/HomeScreen.java
+++ app/src/main/java/com/webteam/wbgapp/wbgapp/activity/HomeScreen.java
@@ -4,8 +4,7 @@ package com.webteam.wbgapp.wbgapp.activity;
  * Created by malte on 01.06.2016.
  */
 public class HomeScreen extends BaseActivity {
-    @Override
-    protected String getName() {
-        return "WbgApp ~ Home";
+    protected String getName(){
+        return "HomeScreen";
     }
 }
